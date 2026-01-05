b57bf99b0de39af7113a114f270df8814e0a1b18
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2026-01-05 07:58:34 +0300

    Fixing build fails

--- migrations/0041_greedy_skrulls.sql
+++ migrations/0041_greedy_skrulls.sql
@@ -1,2 +1,3 @@
 CREATE TYPE "public"."ptl_settings_category" AS ENUM('general', 'security', 'notifications');--> statement-breakpoint
-ALTER TABLE "ptl_settings" ALTER COLUMN "category" SET DATA TYPE ptl_settings_category;
+ALTER TABLE "ptl_settings" ALTER COLUMN "category" SET DATA TYPE ptl_settings_category USING category::ptl_settings_category;--> statement-breakpoint
+ALTER TABLE "ptl_settings" ALTER COLUMN "category" SET DEFAULT 'general';
