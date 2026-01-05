4f3c5ca960418cdf1afd166a8ef166c1f68203e7
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2026-01-05 08:13:18 +0300

    Fixing the recurring build fail

--- migrations/0041_greedy_skrulls.sql
+++ migrations/0041_greedy_skrulls.sql
@@ -1,3 +1,4 @@
 CREATE TYPE "public"."ptl_settings_category" AS ENUM('general', 'security', 'notifications');--> statement-breakpoint
+ALTER TABLE "ptl_settings" ALTER COLUMN "category" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "ptl_settings" ALTER COLUMN "category" SET DATA TYPE ptl_settings_category USING category::ptl_settings_category;--> statement-breakpoint
 ALTER TABLE "ptl_settings" ALTER COLUMN "category" SET DEFAULT 'general'::ptl_settings_category;
